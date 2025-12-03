4eca63b36758f81b97e6872c97e171564ffb5060
Susanne Glassel <glaessel@ikf.uni-frankfurt.de>
2025-12-03 11:26:43 +0100

    small changes

--- batch_runPHQMD.sh
+++ batch_runPHQMD.sh
@@ -117,7 +117,7 @@ if [ "$RunStabilisation" == 1 ]; then
     cd $OUTDIR/$XXXXX
 
     if ! [ -e $phsdFile ]; then                         
-	    echo "Error: ${inputFile} is missing"
+	    echo "Error: ${phsdFile} is missing"
 	    exit 1
     fi
 
